b815a563dc06910f44cc79b6abb21c5e58065afc
gskiyo <g.ykiyo@gmail.com>
2023-06-01 11:41:50 +0900

    MQTT関連とUIのダミーを追加

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")))
 //   res.send('<h1>トップページ!!</h1>');
 // })
 
+// 開閉指示関連
 app.post('/api/v1/quiz', function (req, res) {
     const answer = req.body.answer;
     console.log(answer);
@@ -25,6 +26,22 @@ app.post('/api/v1/quiz', function (req, res) {
     
   })
 
+//   mqtt接続関連
+  app.post('/api/v1/mqttconnect', function (req, res) {
+    const answer = req.body.answer;
+    console.log(answer);
+    console.log("Mqtt process");
+    if(answer === "2"){
+        // res.send("<h1>正解！</h1>");
+        res.redirect("/correct.html");
+    }else{
+        // res.send("<h1>不正解！</h1>");
+        res.redirect("/mqttinfo.html");
+    }
+    
+  })
+
+//   ここで設定値関連をしてみようか
   app.get('/api/v1/users', function (req, res) {
     res.send({
         name: "Mike",
