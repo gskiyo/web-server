792f2e7f6bb3e27e27c66b39c3cd56be07bfcebe
gskiyo <g.ykiyo@gmail.com>
2023-06-01 10:36:24 +0900

    開閉操作画面の作成と開閉指示のUI作成

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.post('/api/v1/quiz', function (req, res) {
     const answer = req.body.answer;
+    console.log(answer);
     if(answer === "2"){
         // res.send("<h1>正解！</h1>");
         res.redirect("/correct.html");
